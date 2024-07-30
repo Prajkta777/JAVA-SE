5d6ed54ffdd19c77f8da574aa01a2a6f63de780f
Prajkta777 <139345654+Prajkta777@users.noreply.github.com>
2024-07-31 00:11:39 +0530

    Add files via upload

---
+++ PropertiesDemo1.java
@@ -0,0 +1,19 @@
+
+
+package propertiesdemo1;
+import java.util.*;
+import java.io.*;
+public class PropertiesDemo1 {
+
+    public static void main(String[] args)throws Exception {
+
+        Properties p=new Properties();
+        p.setProperty("brand","dell");
+        p.setProperty("brand","dell");
+        p.setProperty("brand","dell");
+     //   p.store(new FileOutputStream("C:/Users/Priyanka/Documents/NetBeansProjects/properties.txt"),"Laptop");
+        p.store(new FileOutputStream("C:/Users/Priyanka/Documents/NetBeansProjects/Properties.txt"),"Laptop");
+    
+    }
+    
+}
