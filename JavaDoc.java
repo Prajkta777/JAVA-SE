813a72c3f486b22fff47693682a608b09768b4cb
Prajkta777 <139345654+Prajkta777@users.noreply.github.com>
2024-07-31 00:08:52 +0530

    Add files via upload

---
+++ JavaDoc.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ * @author Priyanka
+ * @version 3.21
+ * @since 2024
+ **/
+
+package javadoc;
+
+/**
+ *
+ * @author Priyanka
+ * class for java documentation demo code
+ */
+public class JavaDoc {
+
+    /**
+     * @value 10 is by default value
+     **/
+   static int val=10;
+   /**
+    * @param bookName book name
+    * */
+
+   
+   public JavaDoc(String bookName)
+   {
+   }
+    /**
+     * @param roll students roll number
+     * @throws Exception if book is not available
+     **/
+   public void issue(int roll)throws Exception{}
+   /**
+    * @param str book name 
+    * @return if book is available then return true else return false
+    */
+   public boolean available(String str)
+   {return true;}
+   /**
+    * 
+    * @param id book id
+    * @return returns book name
+    */
+   
+   public String getName(int id)
+   {return "";}
+   
+   
+}
