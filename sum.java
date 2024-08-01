71ad1075178662b0b0a2776d993b9dd3914b1ac2
Prajkta777 <139345654+Prajkta777@users.noreply.github.com>
2024-08-01 12:31:11 +0530

    Update

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class Sum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+float a,b;
+float c;
+System.out.println("Enter 1st number: ");
+a=nextfloat();
+System.out.println("Enter 1st number: ");
+b=nextfloat();
+c=a+b;
+System.out.println("sum is: "+ c);
+}
+}
